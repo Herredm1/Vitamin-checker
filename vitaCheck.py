ebac7da0c5d7cca7d69aa18bdf6f591017b060cb
Herredm1 <david.m.herrera1989@gmail.com>
2023-04-07 14:29:03 +0800

    Added dynamic supplement list and validation

--- vitaCheck.py
+++ vitaCheck.py
@@ -11,7 +11,7 @@ selectionDict = {
 
 if os.path.exists('supplement.dat'):
     with shelve.open('supplement') as db:
-        userDict = db
+        userDict = db['supplements']
         
 else:
     supplement = 'blank'
@@ -34,61 +34,62 @@ else:
             elif confirm == 'n':
                 break
 
-
-print(userDict)
 def didYouTake(userDict:dict):
-    today = date.today()
-    supplementList = 0
-    while supplementList != len(userDict):
-        for k in userDict.keys():
-            while True:
-                supplement = input(f'Did you take your {k} today (y/n): ').lower()
-                if supplement == 'y':
-                    userDict.update({k:today})
-                    supplementList += 1
-                    break
-                elif supplement == 'n':
-                    input(f'Go take your {k} right now!....')
-                    continue
-                
-        return userDict
-num = 1
-choice = 0
-while choice == 0:
-    print('                 What would you like to do?')
-    
-    for i in selectionDict.values():
-        print(f"{num:>20}.{i}")
-        num += 1
-    
-    num = 1
-    choice = int(input('Selection: '))
-    
-    while choice == 1:
-        os.system('cls')
-        if os.path.exists('userData.dat'):
-            with shelve.open('userData') as db:
-                userDict = db
-                
+    today = str(date.today())
+    for k in userDict.keys():
+        while True:
+            supplement = input(f'Did you take your {k} today (y/n): ').lower()
+            if supplement == 'y':
+                userDict.update({k:today})
+                break
+            while supplement == 'n':
+                input(f'Go take your {k} right now!....')
+                continue
+            else:
+                input('Please choose a valid input......')
+                continue
+    return userDict
+
+choices = [0,1,2,3]
+while True:
+    try:
+        print('                 What would you like to do?')
+        
+        for i in selectionDict.values():
+            print(f"{num:>20}.{i}")
+            num += 1
+        
+        num = 1
+        choice = int(input('Selection: '))
+        
+        while choice == 1:
+            os.system('cls')                
             for k,v in userDict.items():
                 print(f"{k}: {v}")
             
             input("Return to main menu.....")
-            choice =0
-        else:
-            pprint.pprint(userDict)
+            os.system('cls')
             choice = 0
-            input("Return to main menu.....")
-        
-    while choice == 2:
-        userDict = didYouTake(userDict=userDict)
-        with shelve.open('supplement') as db:
-            db = userDict
+
             
+        while choice == 2:
+            userDict = didYouTake(userDict=userDict)
+            with shelve.open('supplement') as db:
+                db['supplements'] = userDict
+            choice = 0
+            os.system('cls')
+                
         while choice == 3:
             exit()
+            
+        if choice not in choices:
+            input("Please choose a valid option....")
+            os.system('cls')
+            choice = 0
+            continue
         
-    if choice not in [1,2,3]:
+    except ValueError:
         input("Please choose a valid option....")
+        os.system('cls')
         choice = 0
         continue
